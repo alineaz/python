3fd9b69a8d3f2e2fee5be68e479fd95d60762dca
Aline Rocha <alinewing@gmail.com>
2024-02-26 18:35:14 -0300

    Arquivos da aula 05

---
+++ aula05.py
@@ -0,0 +1,28 @@
+import csv 
+
+# Trabalhando com arquivos 
+# w - write, faz overwrite em cima do arquivo
+# r - read
+# a - append, adicionar conteudo sem sobescrever
+
+arquivo = open("teste.txt","a")  # abrir arquivo ou criação se não existir
+conteudo = "Primeira linha do meu arquivo.\n" # adicionando conteudo na variavel
+arquivo.write(conteudo) # gravando conteudo no arquivo
+arquivo.close() # fechando arquivo 
+
+arquivo = open("teste.txt","r")
+conteudo = arquivo.read() 
+#print(conteudo)
+arquivo.close()
+
+with open("teste.txt","a") as arquivo:
+    conteudo = "\n Linha adicionada com WITH \n" # adicionando conteudo na variavel
+    arquivo.write(conteudo) # gravando conteudo no arquivo
+print("Finalizado")
+
+# ===========================
+# CSV
+
+with open("arquivo.csv","a") as planilha:
+    conteudo = csv.reader(planilha,delimiter=";")
+    print(conteudo)
